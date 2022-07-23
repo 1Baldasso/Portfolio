01d265eae3c57c3a0aeb23ec90a54487e89d4b20
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2022-07-23 10:37:12 -0300

    trying to fix responsive

--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -11,19 +11,19 @@ export default function ProjectList(){
           <ListItem title={projects.game1.name} description={projects.game1.description} image={projects.game1.Image} url={projects.game1.url} />
         </li>
         <li>
-          <ListItem title={projects.dotNetProject.name} description={projects.dotNetProject.description} image={projects.dotNetProject.Image} url={projects.dotNetProject.url} />
+          <ListItem title={projects.dotNetProject.name} description={projects.dotNetProject.description} image={projects.dotNetProject.Image} url={projects.dotNetProject.url} order="imageFirst"/>
         </li>
         <li>
-          <ListItem title={projects.game2.name} description={projects.game2.description} image={projects.game2.Image} url={projects.game2.url} order="imageFirst" />
+          <ListItem title={projects.game2.name} description={projects.game2.description} image={projects.game2.Image} url={projects.game2.url} />
         </li>
         <li>
-          <ListItem title={projects.game3.name} description={projects.game3.description} image={projects.game3.Image} url={projects.game3.url} />
+          <ListItem title={projects.game3.name} description={projects.game3.description} image={projects.game3.Image} url={projects.game3.url} order="imageFirst" />
         </li>
         <li>
-          <ListItem title={projects.VBAProject.name} description={projects.VBAProject.description} video={projects.VBAProject.Video} url={projects.VBAProject.url} order="imageFirst" />
+          <ListItem title={projects.VBAProject.name} description={projects.VBAProject.description} video={projects.VBAProject.Video} url={projects.VBAProject.url} />
         </li>
         {/*<li>
-          <ListItem title={projects.SBGamesProject.name} description={projects.SBGamesProject.description} image={projects.SBGamesProject.Image} />
+          <ListItem title={projects.SBGamesProject.name} description={projects.SBGamesProject.description} image={projects.SBGamesProject.Image}  order="imageFirst"/>
         </li>*/}
       </ul>
     </div>
